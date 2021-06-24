942a4fdb385fb5a6678895016dac176130b9f96c
raymondmoy <rymoy2@illinois.edu>
2021-06-23 22:11:09 -0500

    Figure out how to make a component and add it to the DOM

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <div> 
+      <Welcome name = 'Ray'/>
+    </div>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );

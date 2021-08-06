5e55594a99ac3b2fe74144cf12f09d93b609c869
raymondmoy <rymoy2@illinois.edu>
2021-08-05 23:10:11 -0500

    Added taskbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import Experience from './pages/Experience'
+import Projects from './pages/Projects'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code></code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/experience' component={Experience} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
